8fb42d737975aec5713294a4009d68d11f69256d
rizkymalm <rizkymalm@gmail.com>
2026-06-13 22:37:25 +0700

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { fileURLToPath } from 'url';
+// import { fileURLToPath } from 'url';
 
-const dirname =
-    typeof __dirname !== 'undefined'
-        ? __dirname
-        : path.dirname(fileURLToPath(import.meta.url));
+// const dirname =
+//     typeof __dirname !== 'undefined'
+//         ? __dirname
+//         : path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
